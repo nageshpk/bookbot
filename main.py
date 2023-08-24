1b65417817d974ca456bdea53069cf0de8eea302
nageshpk <npknagesh@gmail.com>
2023-08-25 03:27:30 +0530

    refactor the code

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+def main():
+    path = 'books/frankenstein.txt'
+    text = read_book(path)
+    num_words = count_words(text)
+    chars = count_letters(text)
+    sorted_list = get_sorted_list(chars)
+    print(f"--- Begin report of {path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+    for item in sorted_list:
+        print(f"The '{item['char']}'  character was found {item['num']} times")
+    print("--- End report ---")
+
+
 def read_book(path):
     with open(path) as f:
         file_contents = f.read()
@@ -19,21 +33,13 @@ def count_letters(string):
     return new_dict
 
 
-path = 'books/frankenstein.txt'
-text = read_book(path)
-num_words = count_words(text)
-chars = count_letters(text)
+def get_sorted_list(chars):
+    sorted_list = []
+    for key, value in chars.items():
+        if key.isalpha():
+            sorted_list.append({"char": key, "num": value})
+    sorted_list.sort(reverse=True, key=lambda dict: dict["num"])
+    return sorted_list
 
-sorted_list = []
-for key, value in chars.items():
-    if key.isalpha():
-        sorted_list.append({"char": key, "num": value})
 
-sorted_list.sort(reverse=True, key=lambda dict: dict["num"])
-
-print(f"--- Begin report of {path} ---")
-print(f"{num_words} words found in the document")
-print()
-for item in sorted_list:
-    print(f"The '{item['char']}'  character was found {item['num']} times")
-print("--- End report ---")
+main()
